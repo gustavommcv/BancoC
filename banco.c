828df931fa80f9f2b8c1bf19d0489ed00f326972
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-15 17:03:44 -0300

    Estruturando projeto

--- banco.c
+++ banco.c
@@ -4,7 +4,90 @@
 #include <Windows.h>
 #include <time.h>
 
+typedef struct {
+    int codigo;
+    char nome[50];
+    char email[50];
+    char cpf[20];
+    char dataNascimento[20];
+    char dataCadastro[20];
+} Cliente;
+
+typedef struct {
+    int numero;
+    Cliente cliente;
+    float saldo;
+    float limite;
+    float saldoTotal; // Saldo + Limite
+} Conta;
+
+void menu();
+void infoCliente(Cliente cliente);
+void infoConta(Conta conta);
+void criarConta();
+void efetuarSaque();
+void efetuarTransferencia();
+void listarContas();
+float atualizaSaldoTotal(Conta conta);
+Conta buscarContaPorNumero(int numero);
+void sacar(Conta conta, float valor);
+void depositar(Conta conta, float valor);
+void transferir(Conta conta_origem, Conta conta_destino, float valor);
+
+static Conta contas[50];
+static int contador_contas = 0;
+static int contador_clientes = 0;
+
 int main(void) {
 
     return 0;
 }
+
+void menu() {
+
+}
+
+void infoCliente(Cliente cliente) {
+
+}
+
+void infoConta(Conta conta) {
+
+}
+
+void criarConta() {
+
+}
+
+void efetuarSaque() {
+
+}
+
+void efetuarTransferencia() {
+
+}
+
+void listarContas() {
+
+}
+
+float atualizaSaldoTotal(Conta conta) {
+
+}
+
+Conta buscarContaPorNumero(int numero) {
+
+}
+
+void sacar(Conta conta, float valor) {
+
+}
+
+void depositar(Conta conta, float valor) {
+
+}
+
+void transferir(Conta conta_origem, Conta conta_destino, float valor) {
+    
+}
+
